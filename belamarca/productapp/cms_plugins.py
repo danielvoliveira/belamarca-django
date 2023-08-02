c16b455f0b45077e9e13ec6b9e17c266aba79b88
Daniel Oliveira <danielvitol@hotmail.com>
2023-08-02 19:11:11 -0300

    Criando regras filtragem ProductCarrosselPlugin5

--- belamarca/productapp/cms_plugins.py
+++ belamarca/productapp/cms_plugins.py
@@ -14,6 +14,7 @@ from .models import (
     ProductPrice,
     ProductCategoryGrid,
     ProductCarrossel,
+    Disponibility,
 )
 
 # ------------------------------------------
@@ -99,22 +100,59 @@ class ProductGridCategory(CMSPluginBase):
 @plugin_pool.register_plugin
 class ProductCarroselPlugin5(CMSPluginBase):
     module = 'Produto'
-    name = '01 - Produtos - Carrosel de Produtos'
+    name = '01 - Produtos - Carrossel de Produtos'
     model = ProductCarrossel
     render_template = "productapp/product_carrossel.html"
     allow_children = False
 
     def render(self, context, instance, placeholder):
 
-        products = instance.products.all()
+        if instance.show_only == 'category':
+            carrossel_products = Product.objects.filter(
+                category__in=instance.categories.all(),
+                productimage__isnull=False, # Apenas produtos que tenham imagem
+                productprice__price__gt=0, # Apenas produtos com preço > 0
+                disp='disponivel'
+            ).order_by('-id')
 
-        for product in products:
+        elif instance.show_only == 'subcategory':
+            carrossel_products = Product.objects.filter(
+                subcategory__id__in=instance.subcategories.all(),
+                productimage__isnull=False, # Apenas produtos que tenham imagem
+                productprice__price__gt=0, # Apenas produtos com preço > 0
+                disp='disponivel'
+            ).order_by('-id')
+
+        elif instance.show_only == 'attribute':
+            carrossel_products = Product.objects.filter(
+                attribute_options__attribute__in=instance.attributes.all(),
+                productimage__isnull=False, # Apenas produtos que tenham imagem
+                productprice__price__gt=0, # Apenas produtos com preço > 0
+                disp='disponivel'
+            ).order_by('-id')
+
+        elif instance.show_only == 'attribute_option':
+            carrossel_products = Product.objects.filter(
+                attribute_options__id__in=instance.attribute_options.all(),
+                productimage__isnull=False, # Apenas produtos que tenham imagem
+                productprice__price__gt=0, # Apenas produtos com preço > 0
+                disp='disponivel'
+            ).order_by('-id')
+
+        elif instance.show_only == 'product':
+            carrossel_products = instance.products.filter(
+                productimage__isnull=False, # Apenas produtos que tenham imagem
+                productprice__price__gt=0, # Apenas produtos com preço > 0
+                disp='disponivel'
+            ).order_by('-id')
+
+        for product in carrossel_products:
             #Pegando preço dos produtos
             product_price = ProductPrice.objects.filter(id_product=product).get()
             product.price = product_price.price
 
         context.update({
             'instance': instance,
-            'products': products,
+            'products': carrossel_products,
         })
         return context
