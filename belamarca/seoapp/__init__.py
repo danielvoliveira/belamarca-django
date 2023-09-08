164a6bada5c2df2a600267421840424ed8f3207f
Daniel Oliveira <danielvitol@hotmail.com>
2023-09-08 20:30:36 -0300

    arquivos conf. p/ SeoApp

---
+++ belamarca/seoapp/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = "seoapp.apps.SeoappConfig"
---
+++ belamarca/seoapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SeoappConfig(AppConfig):
+    name = 'seoapp'
+    verbose_name = 'Configurações seo'
