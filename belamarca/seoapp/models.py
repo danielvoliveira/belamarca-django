ce3c27a5c33bd06d89c5fd41421d4a1cd76d7e08
Daniel Oliveira <danielvitol@hotmail.com>
2023-09-16 18:38:59 -0300

    Ajustando modelos p/ nova metodologia de SEO

--- belamarca/seoapp/models.py
+++ belamarca/seoapp/models.py
@@ -10,6 +10,8 @@ class SocialMediaType(models.TextChoices):
 class GoogleType(models.TextChoices):
     CORPORATION = 'Corporation', 'Corporação'
     ORGANIZATION = 'Organization', 'Organização'
+    PRODUCT_LIST = 'product_list', 'Lista de Produtos'
+    PRODUCT = 'product', 'Página de Produto'
 
 class ShowAddress(models.TextChoices):
     SIM = 'yes', 'Sim'
@@ -259,6 +261,27 @@ class SeoMetaTags(models.Model):
     )
 
     google_type = models.CharField(
+        verbose_name='Modelo de Tags do Google a ser usado?',
+        max_length=12,
+        choices=GoogleType.choices,
+        default=GoogleType.CORPORATION,
+        editable=False,
+    )
+
+    def __str__(self):
+        return self.social_media_title
+
+    class Meta:
+        verbose_name = "SEO Meta Tag"
+        verbose_name_plural = "SEO Meta Tags"
+
+class GoogleCorporationMetaTag(models.Model):
+    seo_meta_tags = models.OneToOneField(
+        SeoMetaTags,
+        on_delete=models.CASCADE
+    )
+
+    google_type = models.CharField(
         verbose_name='Tipo de página',
         max_length=12,
         choices=GoogleType.choices,
@@ -369,8 +392,254 @@ class SeoMetaTags(models.Model):
     )
 
     def __str__(self):
-        return self.path
+        return self.google_type
 
     class Meta:
-        verbose_name = "SEO Meta Tags"
-        verbose_name_plural = "SEO Meta Tags"
+        verbose_name = "Google Corporation Meta Tag"
+        verbose_name_plural = "Google Corporation Meta Tags"
+
+class GoogleProductListMetaTag(models.Model):
+    seo_meta_tags = models.OneToOneField(
+        SeoMetaTags,
+        on_delete=models.CASCADE
+    )
+
+    google_type = models.CharField(
+        verbose_name='Tipo de página',
+        max_length=12,
+        choices=GoogleType.choices,
+        default=GoogleType.PRODUCT_LIST,
+        editable=False,
+    )
+
+    google_name = models.CharField(
+        verbose_name='Nome do site',
+        max_length=100,
+        default='Bela Marca',
+        null=True,
+        blank=True,
+    )
+
+    google_alterantive_name = models.CharField(
+        verbose_name='Nome alternativo do site',
+        max_length=200,
+        default='Bela Marca: para mulheres poderosas como você.',
+        null=True,
+        blank=True,
+    )
+
+    google_description = models.TextField(
+        verbose_name='Descrição do site',
+        max_length=500,
+        default='Bela Marca: para mulheres poderosas como você.',
+        null=True,
+        blank=True,
+    )
+
+    google_logo = models.ImageField(
+        upload_to='seo_images/',
+        default='../../static/assets/logo/logo_quadrado_turquesa.png',
+    )
+
+    google_telephone = models.CharField(
+        verbose_name='Telefone para contato',
+        max_length=20,
+        default='5513997977005',
+        null=True,
+        blank=True,
+    )
+
+    google_address_type = models.CharField(
+        verbose_name='Tipo de endereço',
+        max_length=40,
+        default='postalAddress',
+        null=True,
+        blank=True,
+        editable=False,
+    )
+
+    google_address_street = models.CharField(
+        verbose_name='Endereço',
+        max_length=200,
+        default='',
+        null=True,
+        blank=True,
+    )
+
+    google_address_locality = models.CharField(
+        verbose_name='Cidade',
+        max_length=100,
+        default='Praia Grande',
+        null=True,
+        blank=True,
+    )
+
+    google_address_region = models.CharField(
+        verbose_name='Estado',
+        max_length=100,
+        default='SP',
+        null=True,
+        blank=True,
+    )
+
+    google_social_media_facebook = models.URLField(
+        verbose_name='Link do Facebook',
+        max_length=500,
+        default='',
+        null=True,
+        blank=True,
+    )
+
+    google_social_media_instagram = models.URLField(
+        verbose_name='Link do Instagram',
+        max_length=500,
+        default='https://www.instagram.com/belamarcastore/',
+        null=True,
+        blank=True,
+    )
+
+    google_social_media_youtube = models.URLField(
+        verbose_name='Link do Youtube',
+        max_length=500,
+        default='',
+        null=True,
+        blank=True,
+    )
+
+    google_social_media_tiktok = models.URLField(
+        verbose_name='Link do TikTok',
+        max_length=500,
+        default='',
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.google_type
+
+    class Meta:
+        verbose_name = "Google Product List Meta Tag"
+        verbose_name_plural = "Google Product List Meta Tags"
+
+class GoogleProductMetaTag(models.Model):
+    seo_meta_tags = models.OneToOneField(
+        SeoMetaTags,
+        on_delete=models.CASCADE
+    )
+
+    google_type = models.CharField(
+        verbose_name='Tipo de página',
+        max_length=12,
+        choices=GoogleType.choices,
+        default=GoogleType.PRODUCT,
+        editable=False,
+    )
+
+    google_name = models.CharField(
+        verbose_name='Nome do site',
+        max_length=100,
+        default='Bela Marca',
+        null=True,
+        blank=True,
+    )
+
+    google_alterantive_name = models.CharField(
+        verbose_name='Nome alternativo do site',
+        max_length=200,
+        default='Bela Marca: para mulheres poderosas como você.',
+        null=True,
+        blank=True,
+    )
+
+    google_description = models.TextField(
+        verbose_name='Descrição do site',
+        max_length=500,
+        default='Bela Marca: para mulheres poderosas como você.',
+        null=True,
+        blank=True,
+    )
+
+    google_logo = models.ImageField(
+        upload_to='seo_images/',
+        default='../../static/assets/logo/logo_quadrado_turquesa.png',
+    )
+
+    google_telephone = models.CharField(
+        verbose_name='Telefone para contato',
+        max_length=20,
+        default='5513997977005',
+        null=True,
+        blank=True,
+    )
+
+    google_address_type = models.CharField(
+        verbose_name='Tipo de endereço',
+        max_length=40,
+        default='postalAddress',
+        null=True,
+        blank=True,
+        editable=False,
+    )
+
+    google_address_street = models.CharField(
+        verbose_name='Endereço',
+        max_length=200,
+        default='',
+        null=True,
+        blank=True,
+    )
+
+    google_address_locality = models.CharField(
+        verbose_name='Cidade',
+        max_length=100,
+        default='Praia Grande',
+        null=True,
+        blank=True,
+    )
+
+    google_address_region = models.CharField(
+        verbose_name='Estado',
+        max_length=100,
+        default='SP',
+        null=True,
+        blank=True,
+    )
+
+    google_social_media_facebook = models.URLField(
+        verbose_name='Link do Facebook',
+        max_length=500,
+        default='',
+        null=True,
+        blank=True,
+    )
+
+    google_social_media_instagram = models.URLField(
+        verbose_name='Link do Instagram',
+        max_length=500,
+        default='https://www.instagram.com/belamarcastore/',
+        null=True,
+        blank=True,
+    )
+
+    google_social_media_youtube = models.URLField(
+        verbose_name='Link do Youtube',
+        max_length=500,
+        default='',
+        null=True,
+        blank=True,
+    )
+
+    google_social_media_tiktok = models.URLField(
+        verbose_name='Link do TikTok',
+        max_length=500,
+        default='',
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.google_type
+
+    class Meta:
+        verbose_name = "Google Product Meta Tag"
+        verbose_name_plural = "Google Product Meta Tags"
