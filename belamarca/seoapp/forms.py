9d9e5fe87f08f9d1f0dafbeeae251a18dc78962d
Daniel Oliveira <danielvitol@hotmail.com>
2023-09-16 18:39:21 -0300

    Forms personalizados para SeoApp

--- belamarca/seoapp/forms.py
+++ belamarca/seoapp/forms.py
@@ -1,7 +1,12 @@
 from django.forms.models import ModelForm
+from django import forms
 
 from .models import (
     SeoSinglePagePlugin1,
+    SeoMetaTags,
+    GoogleCorporationMetaTag,
+    GoogleProductListMetaTag,
+    GoogleProductMetaTag,
 )
 
 #------------------------------
@@ -16,3 +21,23 @@ class SeoSinglePagePlugin1Form(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(SeoSinglePagePlugin1Form, self).__init__(*args, **kwargs)
+
+class SeoMetaTagsForm(forms.ModelForm):
+    class Meta:
+        model = SeoMetaTags
+        fields = '__all__'
+
+class GoogleCorporationMetaTagForm(forms.ModelForm):
+    class Meta:
+        model = GoogleCorporationMetaTag
+        fields = '__all__'
+
+class GoogleProductListMetaTagForm(forms.ModelForm):
+    class Meta:
+        model = GoogleProductListMetaTag
+        fields = '__all__'
+
+class GoogleProductMetaTagForm(forms.ModelForm):
+    class Meta:
+        model = GoogleProductMetaTag
+        fields = '__all__'
