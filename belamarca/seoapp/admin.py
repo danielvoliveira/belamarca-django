71d1bf3000fdeaaba7192034625b63a12cfc0fcd
Daniel Oliveira <danielvitol@hotmail.com>
2023-09-16 18:40:09 -0300

    Admin para SeoMetaTags do SeoApp

--- belamarca/seoapp/admin.py
+++ belamarca/seoapp/admin.py
@@ -5,5 +5,41 @@ from .models import (
     SeoMetaTags,
 )
 
-admin.site.register(SeoMetaTags)
+from .models import (
+    SeoMetaTags,
+    GoogleCorporationMetaTag,
+    GoogleProductListMetaTag,
+    GoogleProductMetaTag,
+)
+from .forms import (
+    SeoMetaTagsForm,
+    GoogleCorporationMetaTagForm,
+    GoogleProductListMetaTagForm,
+    GoogleProductMetaTagForm,
+)
+
 admin.site.register(SeoSinglePagePlugin1)
+
+class GoogleCorporationMetaTagInline(admin.StackedInline):
+    model = GoogleCorporationMetaTag
+    form = GoogleCorporationMetaTagForm
+    extra = 0  # Number of empty forms to display
+
+class GoogleProductListMetaTagInline(admin.StackedInline):
+    model = GoogleProductListMetaTag
+    form = GoogleProductListMetaTagForm
+    extra = 0  # Number of empty forms to display
+
+class GoogleProductMetaTagInline(admin.StackedInline):
+    model = GoogleProductMetaTag
+    form = GoogleProductMetaTagForm
+    extra = 0  # Number of empty forms to display
+
+@admin.register(SeoMetaTags)
+class SeoMetaTagsAdmin(admin.ModelAdmin):
+    form = SeoMetaTagsForm
+    inlines = [
+        GoogleCorporationMetaTagInline,
+        GoogleProductListMetaTagInline,
+        GoogleProductMetaTagInline,
+    ]
