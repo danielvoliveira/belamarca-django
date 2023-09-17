8066dec764b8949a2b745eda3d3c4209e010fda8
Daniel Oliveira <danielvitol@hotmail.com>
2023-09-17 16:25:05 -0300

    Regras criação GoogleMetaTags p cada tipo página

--- belamarca/seoapp/signals.py
+++ belamarca/seoapp/signals.py
@@ -7,6 +7,7 @@ from .models import (
     GoogleCorporationMetaTag,
     GoogleProductListMetaTag,
     GoogleProductMetaTag,
+    GoogleType,
 )
 
 # ----------------------------------------
@@ -17,11 +18,40 @@ from .models import (
 @receiver(post_save, sender=SeoMetaTags)
 def create_google_corporation_meta_tag(sender, instance, created, **kwargs):
     if created:
-        if instance.path == 'produtos':
+        if instance.path == '/produtos/':
+            instance.google_type = GoogleType.PRODUCT_LIST
             GoogleProductListMetaTag.objects.create(seo_meta_tags=instance)
-        elif instance.path == 'produto':
+        elif '/produto/' in instance.path:
+            instance.google_type = GoogleType.PRODUCT
             GoogleProductMetaTag.objects.create(seo_meta_tags=instance)
         else:
+            instance.google_type = GoogleType.CORPORATION
             GoogleCorporationMetaTag.objects.create(seo_meta_tags=instance)
+        instance.save()
+    else:
+        if(
+            instance.google_type == 'Corporation' or
+            instance.google_type == 'Organization'
+        ):
+            corporation_tag, created_corporation = GoogleCorporationMetaTag.objects.get_or_create(seo_meta_tags=instance)
+            if not created_corporation:
+                corporation_tag.save()
+
+            GoogleProductListMetaTag.objects.filter(seo_meta_tags=instance).delete()
+            GoogleProductMetaTag.objects.filter(seo_meta_tags=instance).delete()
+        elif instance.google_type == 'product_list':
+            product_list_tag, created_product_list = GoogleProductListMetaTag.objects.get_or_create(seo_meta_tags=instance)
+            if not created_product_list:
+                product_list_tag.save()
+
+            GoogleCorporationMetaTag.objects.filter(seo_meta_tags=instance).delete()
+            GoogleProductMetaTag.objects.filter(seo_meta_tags=instance).delete()
+        elif instance.google_type == 'product':
+            product_tag, created_product = GoogleProductMetaTag.objects.get_or_create(seo_meta_tags=instance)
+            if not created_product:
+                product_tag.save()
+
+            GoogleCorporationMetaTag.objects.filter(seo_meta_tags=instance).delete()
+            GoogleProductListMetaTag.objects.filter(seo_meta_tags=instance).delete()
 
 post_save.connect(create_google_corporation_meta_tag, sender=SeoMetaTags)
